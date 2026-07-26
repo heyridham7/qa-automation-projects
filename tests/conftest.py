95dfcc550b0e24be8013b774f94ee58987bb9c53
Ridham <heyridham7@gmail.com>
2026-07-26 23:22:43 +0530

    Move selenium fixture to tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,17 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    yield driver
-    driver.quit()
+try:
+    from selenium import webdriver
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    @pytest.fixture
+    def driver():
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        yield driver
+        driver.quit()
+
+except ImportError:
+    @pytest.fixture
+    def driver():
+        return None
